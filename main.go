a645f0f319d500c4d72a581e7a41449ae5c4dda8
Joshua Beeler <josh@irisvr.com>
2020-04-14 21:10:07 -0400

    Serve content from /www

--- main.go
+++ main.go
@@ -18,11 +18,13 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	//router.LoadHTMLGlob("templates/*.tmpl.html")
+	//router.Static("/static", "static")
+	router.Static("/www", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		//c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
 	router.Run(":" + port)
