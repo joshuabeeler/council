3edc1b42e3da926811633bec05dc4044534fcdbf
Joshua Beeler <josh@irisvr.com>
2020-04-14 21:19:29 -0400

    Update main.go

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		//c.HTML(http.StatusOK, "index.tmpl.html", nil)
-		c.HTML(http.StatusOK, "index.html", nil)
+		c.HTML(http.StatusOK, "www/index.html", nil)
 	})
 
 	router.Run(":" + port)
