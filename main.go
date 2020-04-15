27777783f453fbfc65f079ecbf49c34be0a89243
Joshua Beeler <josh@irisvr.com>
2020-04-14 21:17:36 -0400

    Update main.go

--- main.go
+++ main.go
@@ -18,9 +18,13 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+
+	// From: https://github.com/gin-gonic/gin#installation
+	router.Use(gin.Recovery())
+
 	//router.LoadHTMLGlob("templates/*.tmpl.html")
 	//router.Static("/static", "static")
-	router.Static("/www", "static")
+	router.Static("/www", "www")
 
 	router.GET("/", func(c *gin.Context) {
 		//c.HTML(http.StatusOK, "index.tmpl.html", nil)
