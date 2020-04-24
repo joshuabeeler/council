7769b1f5ab3ecc5990e40b89d9194e04c1433be6
Joshua Beeler <github@joshuabeeler.com>
2020-04-24 07:02:57 -0400

    Event info specified using time package

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"fmt"
 	"strings"
@@ -35,29 +36,39 @@ func main() {
 		// TODO: Make an event struct type.
 		// TODO: If there isn't a scheduled event, show TBD template info.
 
-		// DATE
-		month := "Apr"
-		day := "22"
-		year := "2020"
+		newYork, err := time.LoadLocation("America/New_York")
+		if (err != nil) {
+			log.Print(err)
+		}
 
-		dayOfWeek := "Wednesday" // TODO: Should be determined by year-month-day, above.
+		startTime := time.Date(2020, 5, 4, 21, 0, 0, 0, newYork)
+		openMinutesBefore, _ := time.ParseDuration("-10m")
+		openTime := startTime.Add(openMinutesBefore)
+		endMinutesAfter, _ := time.ParseDuration("1.5h")
+		endTime := startTime.Add(endMinutesAfter)
 
-		nextDate := fmt.Sprintf(
-			"%s, %s %s, %s",
-			dayOfWeek, month, day, year)
-		
+		//dayOfWeek := "Wednesday" // TODO: Should be determined by year-month-day, above.
+
+		dateStr := startTime.Format("Wednesday, April 22, 2020 (-0700 MST)")
+		log.Print(dateStr)
+
+		//nextDate := fmt.Sprintf(
+		//	"%s, %s %s, %s",
+		//	date.Weekday().String(), month, day, year)
+
 		// OPEN TIME
-		openTime := "5:50 pm"
-		openTimeClean := strings.Replace(openTime, ":", "", -1)
-		openTimeClean = strings.Replace(openTimeClean, " ", "", -1)
-		openTimeClean = strings.ToUpper(openTimeClean)
+		//openTime := "5:50 pm"
+		//openTimeClean := strings.Replace(openTime, ":", "", -1)
+		//openTimeClean = strings.Replace(openTimeClean, " ", "", -1)
+		//openTimeClean = strings.ToUpper(openTimeClean)
+		openTimeClean := openTime.Format("600PM")
 
 		// TODO: Phase out this service. Come up with a better method.
 		// Maybe show PT, MT, CT, and ET in a table, w/ a link for other conversions?
 		// Maybe give folks a drop-down (or search) to convert to their time zone?
 		timeZoneQuery := fmt.Sprintf(
 			"https://time.is/%s_%s_%s_%s_in_Los_Angeles?Online_Council",
-			openTimeClean, day, month, year)
+			openTimeClean, startTime.Day(), startTime.Month(), startTime.Year())
 
 		scheduledEvent := struct {
 			Title string			// TODO: Can probably remove.
@@ -71,13 +82,13 @@ func main() {
 			TimeZoneQuery string
 		}{
 			"Online Council",
-			nextDate,
-			"Pacific time",
-			openTime,		// 8:50 pm ET
-			"6:00 pm",	// 9:00 pm ET
-			"7:30 pm",	// 10:30 pm ET
-			"https://zoom.us/j/93622550259?pwd=ZlpiTmRFUHA4V0tHMHB1cEFubmIwZz09",
-			"936-2255-0259",
+			dateStr,
+			"Eastern time",
+			openTime.Format("6:00 pm"),		// 8:50 pm ET
+			startTime.Format("6:00 pm"),	// 9:00 pm ET
+			endTime.Format("10:30 pm ET"),	// 10:30 pm ET
+			"TODO",
+			"TODO",
 			timeZoneQuery,
 		}
 		
